5f0f2129ffed010645a9f71366f0f95153fb774c
Grischenkov <35846813+Grischenkov@users.noreply.github.com>
2019-11-19 20:32:20 +0300

    1

--- app/src/main/java/com/example/rps_lab_2_3/MainActivity.java
+++ app/src/main/java/com/example/rps_lab_2_3/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rps_lab_2_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
